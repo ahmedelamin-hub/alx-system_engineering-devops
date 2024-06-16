070d9dd39df671bf104b4cca301462382fe630f1
ahmedelamin-hub <ahmedelamin42@gmail.com>
2024-06-16 00:15:47 +0000

    h

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,24 +1,9 @@
-# This Puppet manifest configures Nginx to handle high load by optimizing worker processes and connections.
+# Adjust the maximum number of open files for Nginx and restart the service
 
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  content => template('nginx/nginx.conf.erb'),
-  notify  => Exec['reload-nginx'],
+exec { 'adjust_nginx_open_files_limit':
+  # Modify the configuration file to set the max open files limit to 10000
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  # Define the search path for command execution
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  content => template('nginx/default.erb'),
-  notify  => Exec['reload-nginx'],
-}
-
-exec { 'reload-nginx':
-  command     => '/usr/sbin/nginx -s reload',
-  refreshonly => true,
-}
